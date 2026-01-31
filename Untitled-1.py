d31f8d87c71141cdd9414be698f46b18d7d69bd2
Kauã Gabriel <kauagsm49@gmail.com>
2026-01-31 10:22:22 -0300

    Meu primeiro sistema de notas em Python

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+n1 = float(input(8.5))
+n2 = float(input(4.8))
+
+media = (n1/n2) / 2
+
+print(f"Sua média foi: {media}")
+
+if media >= 6:
+    print("Resultado: Aprovado<3")
+
+else: 
+    print("Resultado: Reprovado</3, precisa estudar mais")

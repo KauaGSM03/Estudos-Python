bb44556ddfb6bdeb358ad986cda1aacccb77f0cd
Kauã Gabriel <kauagsm49@gmail.com>
2026-02-07 09:59:11 -0300

    Finalizando exercicios de Python e organizando arquivos

---
+++ desconto_ssd.py
@@ -0,0 +1,7 @@
+preco_original = float (input("249,90"))
+
+valor_desconto = preco_original * 0.10
+preco_final = preco_original - valor_desconto
+
+print(f"O desconto será de: R$ {valor_desconto}")
+print(f"O valor final com 10% de desconto é: R$ {preco_final}")
